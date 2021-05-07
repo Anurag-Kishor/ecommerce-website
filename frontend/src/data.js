e1d97b4110f2109253b74d6fffde71135dfc6ef1
Anurag <anuragkishor2011@gmail.com>
2021-05-07 20:54:06 +0530

    First commit

---
+++ frontend/src/data.js
@@ -0,0 +1,80 @@
+const data ={
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            images: '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            countInStock:10,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality Product' 
+        },
+        {
+            _id: '2',
+            name: 'Puma Slim Shirt',
+            category: 'Shirts',
+            images: '/images/p2.jpg',
+            price: 150,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality Product', 
+            countInStock:0
+        },
+        {
+            _id: '3',
+            name: 'Polo Slim Shirt',
+            category: 'Shirts',
+            images: '/images/p3.jpg',
+            price: 160,
+            brand: 'Polo',
+            rating: 4.8,
+            numReviews: 10,
+            description: 'high quality Product',
+            countInStock:10,
+ 
+        },
+        {
+            _id: '4',
+            name: 'Nike Pants',
+            category: 'Pants',
+            images: '/images/p4.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality Product',
+            countInStock:10
+
+        },
+        {
+            _id: '5',
+            name: 'Adidas Pants',
+            category: 'Pants',
+            images: '/images/p5.jpg',
+            price: 120,
+            brand: 'Adidas',
+            rating: 4.4,
+            numReviews: 10,
+            countInStock:10,
+            description: 'high quality Product' 
+        },
+        {
+            _id: '6',
+            name: 'Polo Pants',
+            category: 'Pants',
+            images: '/images/p6.jpg',
+            price: 420,
+            brand: 'Polo',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock:10,
+            description: 'high quality Product' 
+        }
+    ]
+};
+
+export default data;
